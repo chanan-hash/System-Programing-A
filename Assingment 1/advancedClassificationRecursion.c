bd08dcddbec9dc8cabe2fe4f4ad76da37c2f0d37
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2024-01-18 21:44:51 +0200

    Adding recursion implementation

---
+++ Assingment 1/advancedClassificationRecursion.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+#include "NumClass.h"
+
+int isPalindrom(int number){
+    if (number == reversRec(number))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isArmstrong (int num){
+    int power = getDigitNum(num);
+    int res = sumOfDigit(num, power);
+    return(num == res ? 1 : 0);
+}
+
+// Helpers function for them
+// we'll check th3e palindrom in the same idea, by reversing the numbers
+
+int reversRec(int rev){
+    int dig = (int) log10 (rev);
+    if (rev == 0)
+    {
+        return 0;
+    }
+    return (rev % 10 * pow(10,dig)) + reversRec(rev / 10); // The recursion call
+}
+
+// Helpers functions for Armstrong number
+
+int getDigitNum(int n){
+    if (n == 0)
+    {
+        return 0;
+    }
+    return 1 + getDigitNum(n/10);
+}
+
+// The power will get from the 'getDigitNum' function
+int sumOfDigit(int number, int power)
+{
+    if (number == 0)
+    {
+        return 0; // This is like to initializ a variable of sum
+    }
+    return (pow(number % 10, power) + sumOfDigit(number/10, power));
+}
